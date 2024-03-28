9cae9e292905f473d555b725095ea9870838254e
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-03-28 17:59:14 +0500

    Add icon generation
    Implemented functionality to automatically generate icons for various themes in Python. Included new IDE icons and updated the .gitignore file.

---
+++ generate/generate.py
@@ -0,0 +1,56 @@
+from time import time
+from io import BytesIO
+from os import listdir, path, makedirs
+
+import cairosvg
+from PIL import Image
+from tqdm import tqdm
+
+time_start = time()
+
+# Color name – background hex
+colors = {
+    'Frappe': '#303446',
+    'Latte': '#eff1f5',
+    'Macchiato': '#24273a',
+    'Mocha': '#1e1e2e'
+}
+
+# An original name – Server name
+icons = {'bash': 'bash', 'batch': 'bat', 'c': 'c', 'cpp': 'cpp', 'csharp': 'csharp', 'database': 'database',
+         'docker-ignore': 'docker-ignore', 'docker': 'docker', '_file': 'file', 'git': 'git', 'go-mod': 'go-mod',
+         'go': 'goland', 'haskell': 'haskell', 'html': 'html', 'http': 'http', 'image': 'image', 'java': 'java',
+         'javascript': 'javascript', 'json': 'json', 'kotlin': 'kotlin', 'markdown': 'markdown', 'perl': 'perl',
+         'php': 'php', 'prisma': 'prisma', 'properties': 'properties', 'python': 'python', 'r': 'r_lang',
+         'ruby': 'ruby', 'rust': 'rust', 'sass': 'sass', 'scala': 'scala', 'svelte': 'svelte', 'svg': 'svg',
+         'swift': 'swift', 'terraform': 'terraform', 'toml': 'toml', 'text': 'txt', 'typescript': 'typescript',
+         'vue': 'vue', 'xml': 'xml', 'yaml': 'yaml'}
+
+# Creating the required folder structure
+for color in colors.keys():
+    if not path.exists('web/' + color):
+        makedirs('web/' + color)
+
+    if not path.exists('web/IDE/' + color):
+        makedirs('web/IDE/' + color)
+
+for theme, background in colors.items():
+    print('* Generating icons ' + theme)
+    for original_icon, cat_icon in tqdm(icons.items()):
+        image_icon = Image.open(BytesIO(cairosvg.svg2png(url=f'vscode-icons/icons/{theme.lower()}/{original_icon}.svg',
+                                                         output_height=1024, output_width=1024)))
+        image_full = Image.new('RGBA', (1536, 1536), background)
+
+        image_full.paste(image_icon, (256, 256), image_icon)
+        image_full.save(f'web/{theme}/{cat_icon}.png')
+
+    print('* Generating IDE icons ' + theme)
+    for file in tqdm(listdir('ide-icons')):
+        image_icon = Image.open(BytesIO(cairosvg.svg2png(url=f'ide-icons/{file}',
+                                                         output_height=1024, output_width=1024)))
+        image_full = Image.new('RGBA', (1536, 1536), background)
+
+        image_full.paste(image_icon, (256, 256), image_icon)
+        image_full.save(f'web/IDE/{theme}/{file.lower().removesuffix('_icon.svg')}.png')
+
+print('[✔] Done. Total time spent:', time() - time_start)
