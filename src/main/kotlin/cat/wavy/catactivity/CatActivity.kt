775f1819a39a516acdc65eb568cff38e0427251e
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-05-24 10:32:04 +0500

    Some changes

--- src/main/kotlin/cat/wavy/catactivity/CatActivity.kt
+++ src/main/kotlin/cat/wavy/catactivity/CatActivity.kt
@@ -2,7 +2,7 @@ package cat.wavy.catactivity
 
 import com.intellij.openapi.diagnostic.Logger
 
-const val ICONS_URL = "https://cat-activity.wavycat.ru" // Without “/“ at the end
+const val ICONS_URL = "https://cat-activity.wavycat.ru"
 
 object CatActivity {
     val logger = Logger.getInstance("Cat Activity")
