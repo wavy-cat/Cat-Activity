9385c7d286092c51e33b2708cc788c40ecf2c999
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-08-09 20:53:24 +0500

    Added new IDE icons in web

--- generate/generate.py
+++ generate/generate.py
@@ -34,9 +34,19 @@ def generate_icons(theme: str, background: str, position: int):
         image_full.save(f'web/{theme}/{cat_icon}.png')
 
 
-def generate_ide_icons(theme: str, background: str, position: int):
-    for file in tqdm(listdir('ide-icons'), position=position, desc='Generating IDE icons ' + theme):
-        image_icon = Image.open(BytesIO(cairosvg.svg2png(url=f'ide-icons/{file}',
+def generate_new_ide_icons(theme: str, background: str, position: int):
+    for file in tqdm(listdir('ide-icons/new'), position=position, desc='Generating new IDE icons ' + theme):
+        image_icon = Image.open(BytesIO(cairosvg.svg2png(url=f'ide-icons/new/{file}',
+                                                         output_height=1024, output_width=1024)))
+        image_full = Image.new('RGBA', (1536, 1536), background)
+
+        image_full.paste(image_icon, (256, 256), image_icon)
+        image_full.save(f'web/IDE/new/{theme}/{file.lower().removesuffix(".svg")}.png')
+
+
+def generate_old_ide_icons(theme: str, background: str, position: int):
+    for file in tqdm(listdir('ide-icons/old'), position=position, desc='Generating old IDE icons ' + theme):
+        image_icon = Image.open(BytesIO(cairosvg.svg2png(url=f'ide-icons/old/{file}',
                                                          output_height=1024, output_width=1024)))
         image_full = Image.new('RGBA', (1536, 1536), background)
 
@@ -51,6 +61,9 @@ for color in colors.keys():
 
     if not path.exists('web/IDE/' + color):
         makedirs('web/IDE/' + color)
+    
+    if not path.exists('web/IDE/new/' + color):
+        makedirs('web/IDE/new/' + color)
 
     if not path.exists('web/index.html'):
         copy('index.html', 'web/index.html')
@@ -60,11 +73,13 @@ if __name__ == '__main__':
 
     for theme, background in colors.items():
         p1 = Process(target=generate_icons, args=(theme, background, len(processes)), daemon=True)
-        p2 = Process(target=generate_ide_icons, args=(theme, background, len(processes) + 1), daemon=True)
+        p2 = Process(target=generate_old_ide_icons, args=(theme, background, len(processes) + 1), daemon=True)
+        p3 = Process(target=generate_new_ide_icons, args=(theme, background, len(processes) + 2), daemon=True)
         p1.start()
         p2.start()
+        p3.start()
 
-        processes += [p1, p2]
+        processes += [p1, p2, p3]
 
     for process in processes:
         process.join()
