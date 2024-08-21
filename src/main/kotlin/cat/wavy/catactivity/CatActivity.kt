2fe414a78c5ff2c06de3d9a455e4234cfcf98fc9
WavyCat <52543076+wavy-cat@users.noreply.github.com>
2024-08-21 21:03:40 +0500

    Update CatActivity.kt

--- src/main/kotlin/cat/wavy/catactivity/CatActivity.kt
+++ src/main/kotlin/cat/wavy/catactivity/CatActivity.kt
@@ -2,7 +2,7 @@ package cat.wavy.catactivity
 
 import com.intellij.openapi.diagnostic.Logger
 
-const val ICONS_URL = "https://cat-activity.wavycat.ru"
+const val ICONS_URL = "https://assets-8nt.pages.dev"
 
 const val NOTIFICATION_GROUP_ID = "Cat Activity"
 
