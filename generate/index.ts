5f9db19e86eee03d1f8387f4b4b20e7c6abcdd80
wavy-cat <me@wavycat.ru>
2025-02-01 16:08:08 +0500

    Add winston to the project

--- generate/index.ts
+++ generate/index.ts
@@ -1,12 +1,22 @@
-import { loadAndProcessConfig, type ProcessedConfig } from 'config';
+import {loadAndProcessConfig, type ProcessedConfig} from "./config"
+import * as winston from "winston"
 
-try {
-    const processedConfig: ProcessedConfig = loadAndProcessConfig('assets_config.json');
-    console.log('Processed Config:', processedConfig);
-} catch (error) {
-    if (error instanceof Error) {
-        console.error('Error processing config:', error.message);
-    } else {
-        console.error('Unexpected error:', error);
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(winston.format.cli()),
+    transports: [
+        new winston.transports.Console(),
+    ],
+})
+
+async function main() {
+    try {
+        const processedConfig: ProcessedConfig = loadAndProcessConfig('assets_config.json')
+        // console.log('Processed Config:', processedConfig);
+    } catch (error) {
+        const e = error as Error
+        logger.crit(`Error when loading config: ${e.message}`)
     }
 }
+
+main().then()
