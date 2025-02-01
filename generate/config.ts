d4f8f12a89df2faa612ab5f4d815c3e5fd4f05a7
wavy-cat <me@wavycat.ru>
2025-02-01 20:32:08 +0500

    Put the fallback icons in a separate structure in the config

--- generate/config.ts
+++ generate/config.ts
@@ -1,6 +1,8 @@
 const fs = require('fs').promises
 import {z} from "zod"
 
+const snowflakeIdRegex = /^\d{0,64}$/g
+
 const RenderSettingsSchema = z.object({
     canvasSize: z.number(),
     iconSize: z.number(),
@@ -9,7 +11,7 @@ const RenderSettingsSchema = z.object({
 const FileIconSchema = z.object({
     title: z.string(),
     altName: z.string().optional(),
-    fileTypes: z.array(z.string()).optional(),
+    fileTypes: z.array(z.string()).nullable().optional(),
     fileNames: z.array(z.string()).nullable().optional(),
     extensions: z.array(z.string()).nullable().optional(),
 })
@@ -17,14 +19,33 @@ const FileIconSchema = z.object({
 const IDEIconSchema = z.object({
     title: z.string(),
     enumName: z.string().optional(),
-    productCodes: z.array(z.string()).nullable().optional(),
-    applicationId: z.string(),
+    productCodes: z.array(z.string().min(2).max(3)).min(1).max(2),
+    applicationId: z.string().regex(snowflakeIdRegex),
+})
+
+const FallbackFileIconSchema = z.object({
+    title: z.string(),
+    icon: z.string(),
+    name: z.string()
+})
+
+const FallbackIDEIconSchema = z.object({
+    title: z.string(),
+    applicationId: z.string().regex(snowflakeIdRegex),
+    enumName: z.string().optional(),
+    icon: z.string(),
+})
+
+const FallbacksSchema = z.object({
+    file: FallbackFileIconSchema,
+    ide: FallbackIDEIconSchema
 })
 
 const ConfigSchema = z.object({
     renderSettings: RenderSettingsSchema,
     fileIcons: z.record(FileIconSchema),
     ideIcons: z.record(IDEIconSchema),
+    fallbacks: FallbacksSchema
 })
 
 export type Config = z.infer<typeof ConfigSchema>
