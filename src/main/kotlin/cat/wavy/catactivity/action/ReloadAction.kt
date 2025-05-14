f8289165932217c987aa1d6002657267c8f43bd5
WavyCat <me@wavycat.ru>
2025-05-14 14:45:11 +0500

    Simplify reload action logging and clean up properties.

--- src/main/kotlin/cat/wavy/catactivity/action/ReloadAction.kt
+++ src/main/kotlin/cat/wavy/catactivity/action/ReloadAction.kt
@@ -1,6 +1,5 @@
 package cat.wavy.catactivity.action
 
-import cat.wavy.catactivity.bundle.ToolsBundle
 import cat.wavy.catactivity.render.ActivityRender
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.service
@@ -10,6 +9,6 @@ import com.intellij.openapi.project.DumbAwareAction
 class ReloadAction : DumbAwareAction() {
     override fun actionPerformed(e: AnActionEvent) {
         service<ActivityRender>().reinit()
-        thisLogger().info(ToolsBundle.message("reloadAction.log.reinitialized"))
+        thisLogger().info("ActivityRender is forcefully reinitialized")
     }
 }
