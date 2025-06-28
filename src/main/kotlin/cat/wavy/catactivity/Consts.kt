529b7cc33246c5d616e72953b7d779a41cbb3cd5
WavyCat <me@wavycat.ru>
2025-06-29 00:41:06 +0500

    fix: update ICONS_URL to new domain

--- src/main/kotlin/cat/wavy/catactivity/Consts.kt
+++ src/main/kotlin/cat/wavy/catactivity/Consts.kt
@@ -1,5 +1,5 @@
 package cat.wavy.catactivity
 
-const val ICONS_URL = "https://cat-activity.wavycat.ru"
+const val ICONS_URL = "https://cat-activity.wcatie.xyz"
 
 const val NOTIFICATION_GROUP_ID = "Cat Activity"
