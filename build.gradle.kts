93f285ac35dfd1f76ecc6f3a7b792849e828a419
WavyCat <me@wavycat.ru>
2025-07-07 15:54:40 +0500

    fix(deps): Change the version of discord-game-sdk4j to v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
 
-    implementation("com.github.JnCrMx:discord-game-sdk4j:-SNAPSHOT")
+    implementation("com.github.JnCrMx:discord-game-sdk4j:v1.0.0")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
