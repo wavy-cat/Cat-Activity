25b7152995ebae322e9b303100c5e8868b1d6fea
WavyCat <root@wavycat.me>
2026-05-02 20:23:25 +0500

    refactor(generate): adjust indentation in `config.ts` and `consts.ts`

--- generate/config.ts
+++ generate/config.ts
@@ -2,50 +2,50 @@ const fs = require('fs').promises
 import {z} from "zod"
 
 const RenderSettingsSchema = z.object({
-    canvasSize: z.number(),
-    iconSize: z.number(),
+  canvasSize: z.number(),
+  iconSize: z.number(),
 })
 
 const FileIconSchema = z.object({
-    title: z.string(),
-    altName: z.string().optional(),
-    fileTypes: z.array(z.string()).nullable().optional(),
-    fileNames: z.array(z.string()).nullable().optional(),
-    extensions: z.array(z.string()).nullable().optional(),
-    enumName: z.string().min(1).optional()
+  title: z.string(),
+  altName: z.string().optional(),
+  fileTypes: z.array(z.string()).nullable().optional(),
+  fileNames: z.array(z.string()).nullable().optional(),
+  extensions: z.array(z.string()).nullable().optional(),
+  enumName: z.string().min(1).optional()
 })
 
 const FileIconsSchema = z.record(
-    z.string(),
-    z.union([
-        FileIconSchema,
-        z.string().transform(str => ({
-            title: str,
-            altName: undefined,
-            fileTypes: undefined,
-            fileNames: undefined,
-            extensions: undefined,
-            enumName: undefined
-        }))
-    ])
+  z.string(),
+  z.union([
+    FileIconSchema,
+    z.string().transform(str => ({
+      title: str,
+      altName: undefined,
+      fileTypes: undefined,
+      fileNames: undefined,
+      extensions: undefined,
+      enumName: undefined
+    }))
+  ])
 )
 
 const ConfigSchema = z.object({
-    renderSettings: RenderSettingsSchema,
-    fileIcons: FileIconsSchema,
-    ignoreIcons: z.array(z.string())
+  renderSettings: RenderSettingsSchema,
+  fileIcons: FileIconsSchema,
+  ignoreIcons: z.array(z.string())
 })
 
 export type Config = z.infer<typeof ConfigSchema>
 
 export async function loadConfig(filepath: string): Promise<Config> {
-    const data = await fs.readFile(filepath, 'utf-8')
-    const parsed = JSON.parse(data)
+  const data = await fs.readFile(filepath, 'utf-8')
+  const parsed = JSON.parse(data)
 
-    const result = await ConfigSchema.safeParseAsync(parsed)
-    if (!result.success) {
-        throw z.treeifyError(result.error)
-    }
+  const result = await ConfigSchema.safeParseAsync(parsed)
+  if (!result.success) {
+    throw z.treeifyError(result.error)
+  }
 
-    return result.data
+  return result.data
 }
--- generate/consts.ts
+++ generate/consts.ts
@@ -1,8 +1,8 @@
 export const Palette = {
-    'Frappe': '#303446',
-    'Latte': '#eff1f5',
-    'Macchiato': '#24273a',
-    'Mocha': '#1e1e2e'
+  'Frappe': '#303446',
+  'Latte': '#eff1f5',
+  'Macchiato': '#24273a',
+  'Mocha': '#1e1e2e'
 }
 
 export const DistFolder = 'assets-dist'
